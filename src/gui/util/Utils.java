7e6646b74410f8b0469c64692738c5cc9e61482a
Jefferson Alves <jefferoonsilva00@gmail.com>
2025-02-27 17:18:18 -0300

    initializing action as parameter

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,23 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+		
+		
+	}
+	
+	public static Integer tryParseToInt(String srt) {
+		try {
+			return Integer.parseInt(srt);
+		}catch(NumberFormatException e) {
+			return null;
+		}
+		
+	}
+}
--- src/model/service/DepartmentService.java
+++ src/model/service/DepartmentService.java
@@ -8,6 +8,7 @@ import model.entities.Department;
 public class DepartmentService {
 
 	public List<Department> findAll(){
+	
 		List<Department> list = new ArrayList<>();
 		list.add(new Department(1, "Books"));
 		list.add(new Department(2, "Computers"));
